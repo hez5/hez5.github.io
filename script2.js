d62bc84c45d93fa26bc41b21aa55a3a282b62f8a
hez5 <50875160+hez5@users.noreply.github.com>
2020-08-28 17:11:01 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,9 @@
 self.addEventListener('message', function(msg){
 	let reeks = new Int32Array(msg.data);
 
-	/*	while (Atomics.load(reeks,0)==42){
+		while (Atomics.load(reeks,0)==42){
 		// Doe niets, wacht tot data is gewijzigd
-	}*/
+	}
 	Atomics.wait(reeks, 0, 15000);
 
 	self.postMessage(`Data gewijzigd naar: ` + Atomics.load(reeks,0));
