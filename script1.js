a66cb5c52bf8ff62166326d7ff746af6f68d69d2
hez5 <50875160+hez5@users.noreply.github.com>
2020-08-31 11:25:46 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,8 +1,8 @@
 self.addEventListener('message', function(msg){
 	let reeks = new Int32Array(msg.data);
-	/*	while (Atomics.load(reeks,0)==42){
+		while (Atomics.load(reeks,0)==42){
 		// Doe niets, wacht tot data is gewijzigd
-	}*/
+	}
 
 	// Wacht maximaal 15sec op een wijziging in de array
 	Atomics.wait(reeks, 0, 15000);
