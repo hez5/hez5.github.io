7d6be7b8e566f7b9dbc4be194962437a0c989d33
hez5 <50875160+hez5@users.noreply.github.com>
2023-01-25 13:33:47 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// code MauraWillems
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {console.log("loading")
+	navigator.serviceWorker.register('.\sw.js')
+	.then(registratie => {console.log(`Registratie gelukt`)});
+});
+};
